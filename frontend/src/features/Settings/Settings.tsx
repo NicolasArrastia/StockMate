a52a0b448b96aecc4fee17d6c7e4a368dc9602f9
Nicolas <nicolasarrastia@gmail.com>
2024-11-01 15:02:58 -0300

    add layout

--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Layout from "../../components/Layout";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return <Layout>Settings</Layout>;
 };
 
 export default Settings;
