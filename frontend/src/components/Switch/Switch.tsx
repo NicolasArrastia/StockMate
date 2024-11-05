d816b5b03fabe0cb699cce0d85cc2d992bcdb515
Nicolas <nicolasarrastia@gmail.com>
2024-11-05 15:11:18 -0300

    wip switch component

---
+++ frontend/src/components/Switch/Switch.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface SwitchProps {
+  isOn: boolean;
+  onToggle: () => void;
+}
+
+const Switch: React.FC<SwitchProps> = ({ isOn, onToggle }) => {
+  return (
+    <div
+      onClick={onToggle}
+      className={`w-10 h-6 flex items-center rounded-full p-1 cursor-pointer ${
+        isOn ? "bg-blue-400" : "bg-gray-300"
+      }`}
+    >
+      <div
+        className={`bg-white w-4 h-4 rounded-full shadow-md transform ${
+          isOn ? "translate-x-4" : "translate-x-0"
+        } transition`}
+      ></div>
+    </div>
+  );
+};
+
+export default Switch;
