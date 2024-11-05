1c3da2d1a3792d9d3e4a77ddcbf375eff645d8aa
Nicolas <nicolasarrastia@gmail.com>
2024-11-05 16:49:12 -0300

    remove unused logic

--- frontend/src/features/Products/pages/EditProduct/EditProduct.tsx
+++ frontend/src/features/Products/pages/EditProduct/EditProduct.tsx
@@ -1,22 +1,7 @@
-import { toast } from "react-toastify";
 import ProductForm from "../../components/ProductForm/ProductForm";
-import { ProductFormData } from "../../types";
-import useUpdateProduct from "../../hooks/useUpdateProduct";
 
 const EditProduct = () => {
-  const { mutate } = useUpdateProduct();
-  const handleUpdateProduct = (data: ProductFormData) => {
-    mutate(data, {
-      onSuccess: () => toast.success("Editado con exitosamente"),
-      onError: (e) => toast.error(e.message),
-    });
-  };
-
-  return (
-    <div>
-      <ProductForm mode="update" onSubmit={handleUpdateProduct} />
-    </div>
-  );
+  return <ProductForm mode="update" />;
 };
 
 export default EditProduct;
--- frontend/src/features/Products/pages/NewProduct/NewProduct.tsx
+++ frontend/src/features/Products/pages/NewProduct/NewProduct.tsx
@@ -1,19 +1,7 @@
-import { toast } from "react-toastify";
 import ProductForm from "../../components/ProductForm/ProductForm";
-import { ProductFormData } from "../../types";
-import useCreateProduct from "../../hooks/useCreateProduct";
 
 const NewProduct = () => {
-  const { mutate } = useCreateProduct();
-
-  const handleCreateProduct = (data: ProductFormData) => {
-    mutate(data, {
-      onSuccess: () => toast.success("Creado"),
-      onError: (e) => toast.error(e.message),
-    });
-  };
-
-  return <ProductForm mode={"create"} onSubmit={handleCreateProduct} />;
+  return <ProductForm mode="create" />;
 };
 
 export default NewProduct;
