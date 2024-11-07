a8a8b2109df1f8638b54e3da682fa40b524901ba
Nicolas <nicolasarrastia@gmail.com>
2024-11-07 10:33:19 -0300

    add generic table

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { flexRender, Table as TableType } from "@tanstack/react-table";
+import Loader from "../Loader";
 
-const Table = () => {
-  return <div>Table</div>;
+type Props<TData> = {
+  tableInstance: TableType<TData>;
+  isLoading: boolean;
+};
+
+const Table = <TData,>({ tableInstance, isLoading }: Props<TData>) => {
+  const { getHeaderGroups, getRowModel } = tableInstance;
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <table className="border w-full">
+          <thead className="bg-neutral-400">
+            {getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id} className="">
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className="first:text-left px-4 py-1">
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {getRowModel().rows.map((row) => (
+              <tr
+                key={row.id}
+                className="odd:bg-neutral-200 hover:bg-neutral-300 group group/row"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="px-4 py-1">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default Table;
