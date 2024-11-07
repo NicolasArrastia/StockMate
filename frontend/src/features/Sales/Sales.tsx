918eb74a40534348a44e992565997278f8be989f
Nicolas <nicolasarrastia@gmail.com>
2024-11-07 10:34:30 -0300

    change title and add mockup

--- frontend/src/features/Sales/Sales.tsx
+++ frontend/src/features/Sales/Sales.tsx
@@ -3,7 +3,8 @@ import Layout from "../../components/Layout";
 const Sales = () => {
   return (
     <Layout>
-      <h1>Sales</h1>
+      <h1>Ventas</h1>
+      TODO add ventas
     </Layout>
   );
 };
--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Layout from "../../components/Layout";
+import Switch from "../../components/Switch";
 
 const Settings = () => {
+  const [isOn, setIsOn] = useState(true);
+
   return (
     <Layout>
-      <h1>Settings</h1>
+      <h1>Ajustes</h1>
+      <div>esto esta en desarrollo, no funciona</div>
+
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-2">
+          <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
+          <span>Activar ayuda en formularios</span>
+        </div>
+        <div className="flex gap-2">
+          <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
+          <span>Activar ayuda en formularios</span>
+        </div>
+      </div>
     </Layout>
   );
 };
