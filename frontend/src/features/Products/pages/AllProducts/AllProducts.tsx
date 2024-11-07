e6c8045ed50ffa59e8e3fc60f0e84f53b9975930
Nicolas <nicolasarrastia@gmail.com>
2024-11-07 13:55:46 -0300

    wip products table

--- frontend/src/features/Products/pages/AllProducts/AllProducts.tsx
+++ frontend/src/features/Products/pages/AllProducts/AllProducts.tsx
@@ -1,11 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Button from "../../../../components/Button";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import useSearchProducts from "../../hooks/useSearchProducts";
-import useProducts from "../../hooks/useGetProducts";
+import useGetProducts from "../../hooks/useGetProducts";
+import Table from "../../../../components/Table";
+import { ProductType } from "../../../../../../types";
+import {
+  ColumnDef,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import SvgIcon from "../../../../components/SvgIcon";
+import { DeleteForeverIcon, EditIcon } from "../../../../assets/svg";
+import TruncatedText from "../../../../components/TruncatedText";
 
 const AllProducts = () => {
-  const { data: products, isLoading, error } = useProducts();
+  const { data: products, isLoading } = useGetProducts();
+
   const [searchTerm, setSearchTerm] = useState("");
   const { data: searchProducts } = useSearchProducts(searchTerm);
 
@@ -19,28 +30,66 @@ const AllProducts = () => {
     navigate("new");
   };
 
-  const renderContent = () => {
-    if (isLoading) return <div>Cargando...</div>;
-    if (error) return <div>Error al cargar los productos: {error.message}</div>;
+  // const { mutate } = useDeleteProduct({
+  //   onSuccess: () => {
+  //     toast.success("Producto borrado");
+  //     queryClient.invalidateQueries({ queryKey: [GET_ALL_PRODUCTS_KEY] });
+  //   },
+  // });
 
-    const displayedProducts = searchTerm ? searchProducts : products;
-
-    return (
-      <ul>
-        {displayedProducts?.map((product) => (
-          <li key={product._id} className="flex">
-            <Link to={product._id} className="underline text-blue-500 mr-2">
-              Edit
-            </Link>
-            <Button onClick={() => {}}>Delete</Button>
-            <span>
-              {product.name} - ${product.price}
-            </span>
-          </li>
-        )) || <div>No hay productos disponibles.</div>}
-      </ul>
-    );
+  const handleEditRedirect = (id: ProductType["_id"]) => {
+    navigate(`/products/${id}`);
   };
+
+  const handleDeleteModal = () => {
+    // TODO: add delete modal
+    alert("TODO");
+  };
+
+  const columns: ColumnDef<ProductType>[] = [
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    { accessorKey: "price", header: "Precio" },
+    {
+      accessorKey: "description",
+      header: "Description",
+      cell: ({
+        row: {
+          original: { description },
+        },
+      }) => <TruncatedText text={description} />,
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => (
+        <div className="flex justify-end">
+          <SvgIcon
+            className="!bg-red-500 cursor-pointer"
+            icon={DeleteForeverIcon}
+            onClick={handleDeleteModal}
+          />
+          <SvgIcon
+            className="!bg-neutral-500 cursor-pointer"
+            icon={EditIcon}
+            onClick={() => handleEditRedirect(row.original._id)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  const displayedProducts = useMemo(
+    () => (searchTerm ? searchProducts : products) || [],
+    [products, searchProducts, searchTerm]
+  );
+
+  const tableInstance = useReactTable({
+    data: displayedProducts,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <>
@@ -48,14 +97,15 @@ const AllProducts = () => {
       <div className="flex items-center justify-between">
         <input
           type="text"
-          placeholder="Buscar productos..."
+          placeholder="Buscar por Nombre, Descripción..."
           value={searchTerm}
           onChange={handleSearch}
-          className="border rounded-md p-2 mb-4"
+          className="border rounded-md p-2 mb-4 w-80"
         />
         <Button onClick={handleCreateProduct}>+ Producto</Button>
       </div>
-      {renderContent()}
+
+      <Table tableInstance={tableInstance} isLoading={isLoading} />
     </>
   );
 };
