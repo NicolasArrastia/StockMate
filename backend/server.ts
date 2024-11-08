73c8e749bce15cab24f084eb4dfc2dc42a5ff660
Nicolas <nicolasarrastia@gmail.com>
2024-11-08 11:08:59 -0300

    migrate to ts

--- backend/server.ts
+++ backend/server.ts
@@ -10,8 +10,8 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json()); // Para parsear JSON
-app.use("/api/products", productRoutes); // Rutas de productos
+app.use(express.json());
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
