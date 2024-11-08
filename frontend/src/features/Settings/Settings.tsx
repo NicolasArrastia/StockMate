3a087e16e128b333b6c17ee4e689f637f1f521cc
Nicolas <nicolasarrastia@gmail.com>
2024-11-08 11:10:31 -0300

    add categories render on settings

--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import Layout from "../../components/Layout";
 import Switch from "../../components/Switch";
+import useGetAllCategories from "../../hooks/settings/categories/useGetAllCategories";
 
 const Settings = () => {
   const [isOn, setIsOn] = useState(true);
+  const { data, isLoading } = useGetAllCategories();
 
+  console.log(data);
   return (
     <Layout>
       <h1>Ajustes</h1>
       <div>esto esta en desarrollo, no funciona</div>
 
+      <h2 className="font-semibold text-xl mt-2">Categorias</h2>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <>{data?.length ? <>no categorias</> : <>hay categorias</>}</>
+      )}
+
+      <h2 className="font-semibold text-xl mt-2">Configuraciones</h2>
       <div className="flex flex-col gap-2">
         <div className="flex gap-2">
           <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
