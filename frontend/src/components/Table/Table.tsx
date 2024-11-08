41020ea8897ab2024014645cd29772bb5969188a
Nicolas <nicolasarrastia@gmail.com>
2024-11-08 11:09:24 -0300

    add no items rendering

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -9,41 +9,52 @@ type Props<TData> = {
 const Table = <TData,>({ tableInstance, isLoading }: Props<TData>) => {
   const { getHeaderGroups, getRowModel } = tableInstance;
 
+  const hasItems = !tableInstance.getRowCount();
+
   return (
     <>
       {isLoading ? (
         <Loader />
       ) : (
-        <table className="border w-full">
-          <thead className="bg-neutral-400">
-            {getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id} className="">
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id} className="first:text-left px-4 py-1">
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </th>
+        <>
+          {hasItems ? (
+            <span>No hay elementos para mostrar</span>
+          ) : (
+            <table className="border w-full">
+              <thead className="bg-neutral-400">
+                {getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id} className="">
+                    {headerGroup.headers.map((header) => (
+                      <th key={header.id} className="first:text-left px-4 py-1">
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </th>
+                    ))}
+                  </tr>
                 ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {getRowModel().rows.map((row) => (
-              <tr
-                key={row.id}
-                className="odd:bg-neutral-200 hover:bg-neutral-300 group group/row"
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id} className="px-4 py-1">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
+              </thead>
+              <tbody>
+                {getRowModel().rows.map((row) => (
+                  <tr
+                    key={row.id}
+                    className="odd:bg-neutral-200 hover:bg-neutral-300 group group/row"
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <td key={cell.id} className="px-4 py-1">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    ))}
+                  </tr>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              </tbody>
+            </table>
+          )}
+        </>
       )}
     </>
   );
