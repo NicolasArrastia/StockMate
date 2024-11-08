5fbf4947780ba75dbc02a259d7ab8dd1e8776c85
Nicolas <nicolasarrastia@gmail.com>
2024-11-08 11:07:32 -0300

    update to typescript

--- backend/models/Product.ts
+++ backend/models/Product.ts
@@ -4,6 +4,12 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
+  quantityOnStock: { type: Number, default: 0 },
+  quantityLowWarning: { type: Number, default: 0 },
+  code: { type: String, required: true },
+
+  // category
+  // tags
 });
 
 const Product = mongoose.model("Product", productSchema);
