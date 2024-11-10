e66e021de228c804f2a2543cf9a5eac120f1c34b
Nicolas <nicolasarrastia@gmail.com>
2024-11-09 23:38:29 -0300

    add colors enum

---
+++ frontend/src/features/Settings/components/Categories/enums.ts
@@ -0,0 +1,67 @@
+// TODO: Move this enum to global folder
+export enum TailwindColors {
+  // NEUTRAL
+  NEUTRAL50 = "#fafafa",
+  NEUTRAL100 = "#f5f5f5",
+  NEUTRAL200 = "#e5e5e5",
+  NEUTRAL300 = "#d4d4d4",
+  NEUTRAL400 = "#a3a3a3",
+  NEUTRAL500 = "#737373",
+  NEUTRAL600 = "#525252",
+  NEUTRAL700 = "#404040",
+  NEUTRAL800 = "#262626",
+  NEUTRAL900 = "#171717",
+  NEUTRAL950 = "#0a0a0a",
+
+  // RED
+  RED50 = "#fef2f2",
+  RED100 = "#fee2e2",
+  RED200 = "#fecaca",
+  RED300 = "#fca5a5",
+  RED400 = "#f87171",
+  RED500 = "#ef4444",
+  RED600 = "#dc2626",
+  RED700 = "#b91c1c",
+  RED800 = "#991b1b",
+  RED900 = "#7f1d1d",
+  RED950 = "#450a0a",
+
+  // GREEN
+  GREEN50 = "#f0fdf4",
+  GREEN100 = "#dcfce7",
+  GREEN200 = "#bbf7d0",
+  GREEN300 = "#86efac",
+  GREEN400 = "#4ade80",
+  GREEN500 = "#22c55e",
+  GREEN600 = "#16a34a",
+  GREEN700 = "#15803d",
+  GREEN800 = "#166534",
+  GREEN900 = "#14532d",
+  GREEN950 = "#052e16",
+
+  // BLUE
+  BLUE50 = "#eff6ff",
+  BLUE100 = "#dbeafe",
+  BLUE200 = "#bfdbfe",
+  BLUE300 = "#93c5fd",
+  BLUE400 = "#60a5fa",
+  BLUE500 = "#3b82f6",
+  BLUE600 = "#2563eb",
+  BLUE700 = "#1d4ed8",
+  BLUE800 = "#1e40af",
+  BLUE900 = "#1e3a8a",
+  BLUE950 = "#172554",
+
+  // YELLOW
+  YELLOW50 = "#fefce8",
+  YELLOW100 = "#fef9c3",
+  YELLOW200 = "#fef08a",
+  YELLOW300 = "#fde047",
+  YELLOW400 = "#facc15",
+  YELLOW500 = "#eab308",
+  YELLOW600 = "#ca8a04",
+  YELLOW700 = "#a16207",
+  YELLOW800 = "#854d0e",
+  YELLOW900 = "#713f12",
+  YELLOW950 = "#422006",
+}
