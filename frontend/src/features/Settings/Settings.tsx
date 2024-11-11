ab3414f29b94ebd0afa753e977001d6bc8a3fc0d
Nicolas <nicolasarrastia@gmail.com>
2024-11-11 13:15:12 -0300

    add category and tags section

--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,36 +1,32 @@
 import { useState } from "react";
 import Layout from "../../components/Layout";
-import Switch from "../../components/Switch";
-import useGetAllCategories from "../../hooks/settings/categories/useGetAllCategories";
+import SwitchInput from "../../components/SwitchInput";
+import Categories from "./components/Categories";
+import Tags from "./components/Tags";
 
 const Settings = () => {
   const [isOn, setIsOn] = useState(true);
-  const { data, isLoading } = useGetAllCategories();
 
-  console.log(data);
   return (
     <Layout>
       <h1>Ajustes</h1>
       <div>esto esta en desarrollo, no funciona</div>
 
-      <h2 className="font-semibold text-xl mt-2">Categorias</h2>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>{data?.length ? <>no categorias</> : <>hay categorias</>}</>
-      )}
+      <Categories />
+      <Tags />
 
       <h2 className="font-semibold text-xl mt-2">Configuraciones</h2>
-      <div className="flex flex-col gap-2">
+      <SwitchInput
+        label={"Activar ayuda en formularios"}
+        isOn={isOn}
+        onToggle={() => setIsOn((prev) => !prev)}
+      />
+      {/* <div className="flex flex-col gap-2">
         <div className="flex gap-2">
           <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
           <span>Activar ayuda en formularios</span>
         </div>
-        <div className="flex gap-2">
-          <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
-          <span>Activar ayuda en formularios</span>
-        </div>
-      </div>
+      </div> */}
     </Layout>
   );
 };
