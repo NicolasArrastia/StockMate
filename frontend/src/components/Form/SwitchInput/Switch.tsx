024510bd822507fdb01bdeb9c166dbb817a718e6
Nicolas <nicolasarrastia@gmail.com>
2024-11-14 15:40:28 -0300

    add new refactored switch for forms

---
+++ frontend/src/components/Form/SwitchInput/Switch.tsx
@@ -0,0 +1,37 @@
+import {
+  Control,
+  Controller,
+  ControllerRenderProps,
+  FieldValues,
+  Path,
+} from "react-hook-form";
+import SwitchUI from "../../Switch";
+
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  control: Control<T>;
+  label?: string;
+};
+
+const Switch = <T extends FieldValues>({ name, control, label }: Props<T>) => {
+  const handleToggle = (field: ControllerRenderProps<T>) => {
+    field.onChange(!field.value);
+  };
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => {
+        return (
+          <div className="flex gap-2">
+            <SwitchUI onToggle={() => handleToggle(field)} isOn={field.value} />
+            <span>{label}</span>
+          </div>
+        );
+      }}
+    />
+  );
+};
+
+export default Switch;
