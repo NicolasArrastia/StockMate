2a32b00fe7ee113314f31a0b9f2969461060a0b9
Nicolas <nicolasarrastia@gmail.com>
2024-11-14 17:26:09 -0300

    make type global

--- frontend/src/features/Products/productRoutes.tsx
+++ frontend/src/features/Products/productRoutes.tsx
@@ -1,12 +1,7 @@
-import { ReactElement } from "react";
 import AllProducts from "./pages/AllProducts";
 import NewProduct from "./pages/NewProduct";
 import EditProduct from "./pages/EditProduct";
-
-type RouteType = {
-  path: string;
-  element: ReactElement;
-};
+import { RouteType } from "../../types";
 
 export const productRoutes: RouteType[] = [
   { path: "/", element: <AllProducts /> },
