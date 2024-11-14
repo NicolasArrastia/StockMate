851709933afa9f3f4a2c7c5b4589e858915ebd96
Nicolas <nicolasarrastia@gmail.com>
2024-11-14 16:11:09 -0300

    add lazy load with suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,33 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Products from "./features/Products";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Sales from "./features/Sales";
-import Dashboard from "./features/Dashboard";
-import NotFound from "./features/NotFound";
-import Settings from "./features/Settings";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
 
 const queryClient = new QueryClient();
+
+const Products = lazy(() => import("./features/Products"));
+const Sales = lazy(() => import("./features/Sales"));
+const Dashboard = lazy(() => import("./features/Dashboard"));
+const Settings = lazy(() => import("./features/Settings"));
+const NotFound = lazy(() => import("./features/NotFound"));
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/products" replace />} />
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/products" replace />} />
 
-          <Route path="/products/*" element={<Products />} />
-          <Route path="/sales/*" element={<Sales />} />
-          <Route path="/dashboard/*" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            <Route path="/products/*" element={<Products />} />
+            <Route path="/sales/*" element={<Sales />} />
+            <Route path="/dashboard/*" element={<Dashboard />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <ToastContainer draggable theme="colored" />
     </QueryClientProvider>
