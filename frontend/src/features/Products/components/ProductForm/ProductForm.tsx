3102cdc9c4008fe318758043b37c2099ca4235e4
Nicolas <nicolasarrastia@gmail.com>
2024-11-14 16:07:19 -0300

    organize fields

--- frontend/src/features/Products/components/ProductForm/ProductForm.tsx
+++ frontend/src/features/Products/components/ProductForm/ProductForm.tsx
@@ -1,13 +1,13 @@
-import Input from "../../../../components/Input";
+import Input from "../../../../components/Form/Input";
 import Button from "../../../../components/Button";
 import { ProductPopulatedType } from "@globalTypes/types";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import Loader from "../../../../components/Loader";
 import useGetAllCategories from "../../../../hooks/settings/categories/useGetAllCategories";
 import SelectInput from "../../../../components/SelectInput";
-import useGetAllTags from "../../../../hooks/settings/tags/useGetAllTags";
-import SwitchInput from "../../../../components/SwitchInput";
 import useProductForm from "./useProductForm";
+import ProfitValue from "./components/ProfitValue/ProfitValue";
+import Switch from "../../../../components/Form/Switch";
 
 type ProductFormFields = Partial<ProductPopulatedType> & {
   _id: ProductPopulatedType["_id"];
@@ -23,11 +23,11 @@ const SECTION_CLASSES = "bg-neutral-200 p-4 rounded-sm gap-8";
 
 // TODO: create useProductForm hook for handling logic
 const ProductForm = ({ mode }: Props) => {
-  const { buttons, getValues, isLoading, register } = useProductForm({ mode });
-
+  const { buttons, getValues, isLoading, register, watch, control } =
+    useProductForm({
+      mode,
+    });
   const { data: categories } = useGetAllCategories();
-  const [tagSearch, setTagSearch] = useState("");
-  const { data: tags } = useGetAllTags({ search: tagSearch });
 
   const formattedCategories = useMemo(() => {
     return (
@@ -48,30 +48,9 @@ const ProductForm = ({ mode }: Props) => {
           onSubmit={(e) => e.preventDefault()}
         >
           <h1>{getValues("name")}</h1>
-          <div className={`${SECTION_CLASSES} grid grid-cols-3`}>
-            <Input<ProductFormFields>
-              label="Precio"
-              name="price"
-              register={register}
-              type="number"
-            />
-            <Input<ProductFormFields>
-              label="Precio Coste"
-              name="costPrice"
-              register={register}
-              type="number"
-            />
-            <div>
-              {Number(getValues("costPrice")) && Number(getValues("price"))
-                ? `${(
-                    (Number(getValues("costPrice")) * 100) /
-                    Number(getValues("price"))
-                  ).toFixed(2)}%`
-                : null}
-            </div>
-          </div>
+
           <div
-            className={`grid grid-cols-3 grid-rows-2 gap-8 ${SECTION_CLASSES}`}
+            className={`grid grid-cols-4 items-center gap-8 ${SECTION_CLASSES}`}
           >
             <Input<ProductFormFields>
               label="Nombre"
@@ -89,27 +68,41 @@ const ProductForm = ({ mode }: Props) => {
               register={register}
               options={formattedCategories}
             />
+            <Switch
+              name="isActive"
+              control={control}
+              label="es un producto activo"
+            />
+          </div>
 
-            <div className="flex flex-col">
-              <label
-                htmlFor="tags"
-                className="text-sm font-semibold text-gray-600 uppercase"
-              >
-                Add a tag:
-              </label>
-              <input
-                list="tag-options"
-                id="tags"
-                name="tags"
-                placeholder="Select or type a tag"
-                className="p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          <div className={`${SECTION_CLASSES} grid grid-cols-4 items-center`}>
+            <Input<ProductFormFields>
+              label="Precio"
+              name="price"
+              register={register}
+              type="number"
+            />
+            <Input<ProductFormFields>
+              label="Precio Coste"
+              name="costPrice"
+              register={register}
+              type="number"
+            />
+            <div>
+              <span className="block text-sm font-medium mb-1 uppercase text-neutral-600">
+                Ganancia
+              </span>
+              <ProfitValue
+                price={watch("price")}
+                costPrice={watch("costPrice")}
               />
-              <datalist id="tag-options">
-                {tags?.map((tag) => (
-                  <option key={tag._id} value={tag.name} />
-                ))}
-              </datalist>
             </div>
+
+            <Switch<ProductFormFields>
+              control={control}
+              name="ignorePrice"
+              label="Ignorar Precio"
+            />
           </div>
 
           <div
@@ -125,20 +118,22 @@ const ProductForm = ({ mode }: Props) => {
               name="lowQuantityWarning"
               register={register}
             />
-            <SwitchInput
-              label="Ignorar cantidad"
-              isOn={false}
-              onToggle={() => {}}
+            <Switch<ProductFormFields>
+              control={control}
+              name="ignoreQuantity"
+              label="Ignorar Cantidad"
             />
           </div>
 
-          <div className={`${SECTION_CLASSES}`}>
-            <div>supplier</div>
-            <div className="mt-2">
-              <span className="p-2 rounded-full bg-neutral-400 font-semibold text-neutral-100">
-                LM
-              </span>{" "}
-              <span>Luis Marroquin</span>
+          <div className="grid grid-cols-2 gap-4">
+            <div className={`${SECTION_CLASSES}`}>
+              <div>supplier</div>
+              <div className="mt-2">
+                <span className="p-2 rounded-full bg-neutral-400 font-semibold text-neutral-100">
+                  LM
+                </span>{" "}
+                <span>Luis Marroquin</span>
+              </div>
             </div>
           </div>
 
