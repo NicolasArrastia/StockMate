1ca41bdcee88e53b7c66d52f8b386a37da334dbf
Nicolas <nicolasarrastia@gmail.com>
2024-11-15 19:03:18 -0300

    wip improve settings content

--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,26 +1,18 @@
-import { useState } from "react";
 import Layout from "../../components/Layout";
 import Categories from "./components/Categories";
 import Tags from "./components/Tags";
 
 const Settings = () => {
-  const [isOn, setIsOn] = useState(true);
-
   return (
     <Layout>
       <h1>Ajustes</h1>
       <div>esto esta en desarrollo, no funciona</div>
-
       <Categories />
       <Tags />
-
       <h2 className="font-semibold text-xl mt-2">Configuraciones</h2>
-      {/* <div className="flex flex-col gap-2">
-        <div className="flex gap-2">
-          <Switch isOn={isOn} onToggle={() => setIsOn((prev) => !prev)} />
-          <span>Activar ayuda en formularios</span>
-        </div>
-      </div> */}
+      <span>Activar ayuda en formularios</span>
+      <h3>Ventas</h3>
+      <span>Metodo de pago por defecto</span>
     </Layout>
   );
 };
