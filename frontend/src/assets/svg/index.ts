f5cbf9834d3da98a545dd39a0292e4e756159c2f
Nicolas <nicolasarrastia@gmail.com>
2024-11-15 18:25:22 -0300

    add new icons

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -10,6 +10,19 @@ import DeleteForeverIcon from "./delete_forever.svg";
 import EditIcon from "./edit.svg";
 import LoaderIcon from "./loader.svg";
 import WarningIcon from "./warning.svg";
+import CheckCircleIcon from "./check_circle.svg";
+import CancelIcon from "./cancel.svg";
+import ScheduleIcon from "./schedule.svg";
+import ErrorIcon from "./error.svg";
+import AddIcon from "./add.svg";
+import AddCircleIcon from "./add_circle.svg";
+
+import HelpIcon from "./help.svg";
+import LanguageIcon from "./language.svg";
+import PaymentsIcon from "./payments.svg";
+import CreditCardIcon from "./credit_card.svg";
+import SellIcon from "./sell.svg";
+import BarcodeScannerIcon from "./barcode_scanner.svg";
 
 export {
   InventoryIcon,
@@ -24,4 +37,16 @@ export {
   EditIcon,
   LoaderIcon,
   WarningIcon,
+  CheckCircleIcon,
+  CancelIcon,
+  ScheduleIcon,
+  ErrorIcon,
+  AddIcon,
+  AddCircleIcon,
+  HelpIcon,
+  LanguageIcon,
+  PaymentsIcon,
+  CreditCardIcon,
+  SellIcon,
+  BarcodeScannerIcon,
 };
