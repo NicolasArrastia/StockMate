02a7edcb7cd99e17bbcc135244a556b5b0517bc8
Nicolas <nicolasarrastia@gmail.com>
2024-11-15 18:48:06 -0300

    move and create sections in sidebar

--- frontend/src/components/Layout/components/Sidebar/constants.ts
+++ frontend/src/components/Layout/components/Sidebar/constants.ts
@@ -1,23 +1,25 @@
 import {
   AttachMoneyIcon,
   DashboardIcon,
-  DescriptionIcon,
-  GroupsIcon,
   InventoryIcon,
+  GroupsIcon,
   LocalShippingIcon,
   SettingsIcon,
-} from "../../../../assets/svg";
+  DescriptionIcon,
+  SellIcon,
+} from "src/assets/svg";
 
 export const SIDEBAR_OPTIONS: { path: string; label: string; icon: string }[] =
   [
     // MVP
     { path: "/products", label: "Productos", icon: InventoryIcon },
     { path: "/sales", label: "Ventas", icon: AttachMoneyIcon },
-    { path: "/settings", label: "Ajustes", icon: SettingsIcon },
     { path: "/dashboard", label: "Dashboard", icon: DashboardIcon },
 
     // MDP
     { path: "/clients", label: "Clientes", icon: GroupsIcon },
     { path: "/suppliers", label: "Proveedores", icon: LocalShippingIcon },
+    { path: "/settings", label: "Ajustes", icon: SettingsIcon },
     { path: "/docs", label: "Documentación", icon: DescriptionIcon },
+    { path: "/offers", label: "Ofertas", icon: SellIcon },
   ];
