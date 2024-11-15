755f2d083bde1b164817703b00a46a7c91aecdd0
Nicolas <nicolasarrastia@gmail.com>
2024-11-14 21:33:43 -0300

    add path alias for components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@globalTypes": path.resolve(__dirname, "../types"),
+      "@components": path.resolve(__dirname, "src/components"),
+    },
   },
 });
