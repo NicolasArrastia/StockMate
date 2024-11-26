52b004cf7ac1b06c13ed7b7e005576ffe5c1930b
Nicolas <nicolasarrastia@gmail.com>
2024-11-26 15:38:37 -0300

    Fix routing when running app as executable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { lazy, Suspense } from "react";
@@ -14,21 +20,40 @@ const Settings = lazy(() => import("./features/Settings"));
 const NotFound = lazy(() => import("./features/NotFound"));
 
 const App = () => {
+  // TODO: Refactor this logic
+  const isProduction = import.meta.env.VITE_ENV === "production";
+
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<Navigate to="/products" replace />} />
+      {isProduction ? (
+        <HashRouter>
+          <Suspense fallback={<>loading...</>}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" replace />} />
 
-            <Route path="/products/*" element={<Products />} />
-            <Route path="/sales/*" element={<Sales />} />
-            <Route path="/dashboard/*" element={<Dashboard />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
+              <Route path="/products/*" element={<Products />} />
+              <Route path="/sales/*" element={<Sales />} />
+              <Route path="/dashboard/*" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </HashRouter>
+      ) : (
+        <BrowserRouter>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Navigate to="/products" replace />} />
+
+              <Route path="/products/*" element={<Products />} />
+              <Route path="/sales/*" element={<Sales />} />
+              <Route path="/dashboard/*" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      )}
       <ToastContainer draggable theme="colored" />
     </QueryClientProvider>
   );
